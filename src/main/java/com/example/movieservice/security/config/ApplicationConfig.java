3f9b42502f8f7a2c61679018594ae9d66d818c6c
Coda Victor <vitmax.coda@gmail.com>
2023-09-14 11:26:11 +0300

    Configuratiile la Tomcat si adaugarea SSL

--- src/main/java/com/example/movieservice/security/config/ApplicationConfig.java
+++ src/main/java/com/example/movieservice/security/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.example.movieservice.security.config;
 
 
 import com.example.movieservice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
---
+++ src/main/java/com/example/movieservice/security/config/TomcatConfig.java
@@ -0,0 +1,44 @@
+package com.example.movieservice.security.config;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TomcatConfig {
+
+    @Value("${server.port}")
+    private int serverPort;
+
+    @Bean
+    public ServletWebServerFactory servletContainer(){
+        TomcatServletWebServerFactory tomcat =new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context){
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection=new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }
+
+    private Connector initiateHttpConnector() {
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8082);
+        connector.setSecure(false);
+        connector.setRedirectPort(serverPort);
+        return connector;
+    }
+}
